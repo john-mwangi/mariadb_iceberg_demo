4d14d688e489fb7ea79cb5859b3209d29c7ed834
John Mwangi <mail.mwangi@gmail.com>
2024-09-07 09:52:02 +0000

    Automate jobs and service

---
+++ dockerfiles/scripts/create_jobs.sql
@@ -0,0 +1,40 @@
+SET execution.checkpointing.interval = '3s';
+
+CREATE TABLE user_source (
+    database_name STRING METADATA VIRTUAL,
+    table_name STRING METADATA VIRTUAL,
+    `id` DECIMAL(20, 0) NOT NULL,
+    name STRING,
+    address STRING,
+    phone_number STRING,
+    email STRING,
+    PRIMARY KEY (`id`) NOT ENFORCED
+  ) WITH (
+    'connector' = 'mysql-cdc',
+    'hostname' = 'mariadb',
+    'port' = '3306',
+    'username' = 'root',
+    'password' = 'mypass',
+    'database-name' = 'db_[0-9]+',
+    'table-name' = 'user_[0-9]+'
+  );
+
+
+CREATE TABLE all_users_sink (
+    database_name STRING,
+    table_name    STRING,
+    `id`          DECIMAL(20, 0) NOT NULL,
+    name          STRING,
+    address       STRING,
+    phone_number  STRING,
+    email         STRING,
+    PRIMARY KEY (database_name, table_name, `id`) NOT ENFORCED
+  ) WITH (
+    'connector'='iceberg',
+    'catalog-name'='iceberg_catalog',
+    'catalog-type'='hadoop',  
+    'warehouse'='file:///tmp/iceberg/warehouse',
+    'format-version'='2'
+  );
+
+INSERT INTO all_users_sink select * from user_source;
