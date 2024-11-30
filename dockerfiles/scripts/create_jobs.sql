d0018654d665973b0fc9c533c310ac25ca70c746
John Mwangi <mail.mwangi@gmail.com>
2024-11-30 03:41:28 +0000

    Add working Paimon Kafka Table Sync Action

--- dockerfiles/scripts/create_jobs.sql
+++ dockerfiles/scripts/create_jobs.sql
@@ -102,7 +102,36 @@ INSERT INTO all_users_sink_kafka SELECT * FROM user_source_kafka;
 SELECT * FROM all_users_sink_kafka;
 
 -- *************************************************
--- ******    PAIMON KAFKA SYNC ACTION    ***********
+-- *****    PAIMON KAFKA TABLE SYNC ACTION    ******
+-- *************************************************
+
+
+CREATE CATALOG paimon_catalog WITH (
+    'type' = 'paimon',
+    'warehouse' = 'file:///tmp/paimon/warehouse'
+);
+
+
+docker exec -ti jobmanager bash
+
+# Synchronisation of one Paimon table to one Kafka topic.
+flink run \
+    /opt/flink/lib/paimon-flink-action-1.0-20241111.002633-54.jar \
+    kafka_sync_table \
+    --warehouse file:///tmp/paimon/warehouse \
+    --database users_ta \
+    --table user_2 \
+    --primary_keys id \
+    --kafka_conf properties.bootstrap.servers=kafka:9092 \
+    --kafka_conf topic=users.db_1.user_2 \
+    --kafka_conf value.format=debezium-json \
+    --table_conf changelog-producer=input \
+    --kafka_conf scan.startup.mode=earliest-offset
+
+SELECT * FROM paimon_catalog.users_ta.user_2;
+
+-- *************************************************
+-- ******    PAIMON KAFKA DB SYNC ACTION    ********
 -- *************************************************
 
 CREATE CATALOG paimon_catalog WITH (
@@ -117,7 +146,7 @@ flink run \
     /opt/flink/lib/paimon-flink-action-1.0-20241111.002633-54.jar \
     kafka_sync_database \
     --warehouse file:///tmp/paimon/warehouse \
-    --database users \
+    --database users_da \
     --kafka_conf properties.bootstrap.servers=kafka:9092 \
     --kafka_conf topic-pattern=users\.db_[0-9]+\.user_[0-9]+ \
     --kafka_conf value.format=debezium-json \
